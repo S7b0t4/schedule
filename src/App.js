e952de3f15b57195bd3febf93a9dbf0f489bbf71
S7b0t4 <s7b0t4@MacBook-Pro-S7b0t4.local>
2024-02-25 03:34:29 +0500

    gix some bug again

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
 import "./App.css"
-
 import Management from './Component/Management';
 import Schedule from './Component/Schedule';
 
 const App = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
-
-  const [groupIndex, setGroupIndex] = useState(1)
-
-  const [schedule, setSchedule] = useState()
-
+  const [groupIndex, setGroupIndex] = useState(1);
+  const [schedule, setSchedule] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
   const [groupNow, setGroupNow] = useState("select");
 
   const getDayOfWeek = () => {
@@ -23,52 +17,60 @@ const App = () => {
     return daysOfWeek[dayIndex];
   }
   
-  
   const day = currentDate.getDate();
   const wday = getDayOfWeek();
   const month = currentDate.getMonth() + 1;
   const year = currentDate.getFullYear();
 
-  let url = `https://ca7541dbbd4adbd78f4d56f7abbf11d0.serveo.net/`
+  const url = `https://ca7541dbbd4adbd78f4d56f7abbf11d0.serveo.net/`;
 
-  function postData() {
-    console.log("getData")
-    axios.post(url+`post`, {"day":day, "month":month, "year":year})
-    .then((response) => {
-      setSchedule(response.data);
-      setIsLoading(false);
-    })
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(`${url}post`, {"day":day, "month":month, "year":year});
+        setSchedule(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      }
+    };
 
-  useEffect((postData) => {
-    postData()
-  }, [currentDate])
-
-  console.log(schedule)
+    fetchData();
+  }, [currentDate, day, month, year, url]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
-  if(schedule && schedule.length > 3){
-    return (
-      <div className='main_block'>
-        <div>
-            <Management groupNow={groupNow} setGroupNow={(v)=>setGroupNow(v)} setGroupIndex={(v)=>{setGroupIndex(v)}} group={schedule[1]} setCurrentDate={(v) => setCurrentDate(v)} wday={wday} day={day} month={month} year={year}/>
-            <Schedule isLoading={isLoading} groupIndex={groupIndex} schedule={schedule}/>
-          </div>
-      </div>
-    )
+
+  if (!schedule) {
+    return <div>No data available</div>;
   }
-  if(schedule && schedule.length <= 3){
-    return (
-      <div className='main_block'>
-        <div>
-            <Management groupNow={groupNow} setGroupNow={(v)=>setGroupNow(v)} setCurrentDate={(v) => setCurrentDate(v)} wday={wday} day={day} month={month} year={year}/>
-            <div>{schedule[0]}</div>
-          </div>
+
+  const isFullSchedule = schedule.length > 3;
+
+  return (
+    <div className='main_block'>
+      <div>
+        <Management
+          groupNow={groupNow}
+          setGroupNow={setGroupNow}
+          setGroupIndex={setGroupIndex}
+          group={isFullSchedule ? schedule[1] : null}
+          setCurrentDate={setCurrentDate}
+          wday={wday}
+          day={day}
+          month={month}
+          year={year}
+        />
+        {isFullSchedule ? (
+          <Schedule isLoading={isLoading} groupIndex={groupIndex} schedule={schedule} />
+        ) : (
+          <div>{schedule[0]}</div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;

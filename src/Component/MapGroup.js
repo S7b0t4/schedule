8f555fef187a40f7c1af2fd66a0178e9579dfa1a
S7b0t4 <s7b0t4@MacBook-Pro-S7b0t4.local>
2024-02-25 03:36:31 +0500

    gix some bug again

--- src/Component/MapGroup.js
+++ src/Component/MapGroup.js
@@ -1,33 +1,46 @@
 import React, { useState } from 'react';
 
-const MapGroup = ({groupNow, setGroupNow, setGroupIndex, group}) => {
-  const [vive, setVive] = useState(false)
+const MapGroup = ({ groupNow, setGroupNow, setGroupIndex, group }) => {
+  const [vive, setVive] = useState(false);
 
   const changeVive = () => {
-    setVive(!vive)
+    setVive(!vive);
+  };
+
+  if (!group) {
+    return null;
   }
 
-  if(group){
-    return(
-      <div className='group_block'>
-        <div className='group_item' onClick={()=>{changeVive()}}>
-          {groupNow}
-        </div>
-        {vive && 
-          <div className='group_colum'>{
-            group.map((element, index) => {
-              let color = (index % 2 === 1) ? "gray" : "white";
-              if(element !== "ВРЕМЯ"){
-                return(
-                  <div key={index} className={"group_item " + color } onClick={()=>{setGroupIndex(index); setGroupNow(element); changeVive()}}>{element}</div>
-                )
-              }
-            })
-          }</div>
-        }
+  return (
+    <div className='group_block'>
+      <div className='group_item' onClick={changeVive}>
+        {groupNow}
       </div>
-    )
-  }
-}
+      {vive && (
+        <div className='group_colum'>
+          {group.map((element, index) => {
+            if (element !== "ВРЕМЯ") {
+              let color = index % 2 === 1 ? "gray" : "white";
+              return (
+                <div
+                  key={index} // Add key prop here
+                  className={"group_item " + color}
+                  onClick={() => {
+                    setGroupIndex(index);
+                    setGroupNow(element);
+                    changeVive();
+                  }}
+                >
+                  {element}
+                </div>
+              );
+            }
+            return null; // Add this line to handle the case when element is "ВРЕМЯ"
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MapGroup;

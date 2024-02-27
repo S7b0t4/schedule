a8b61581bb3026d5a550947cbb51103888664431
S7b0t4 <98168529+S7b0t4@users.noreply.github.com>
2024-02-27 16:53:39 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   const month = currentDate.getMonth() + 1;
   const year = currentDate.getFullYear();
 
-  const url = `https://glossy-mini-pram.glitch.me/`;
+  const url = `http://46.36.217.193:5000/`;
 
   useEffect(() => {
     const fetchData = async () => {
